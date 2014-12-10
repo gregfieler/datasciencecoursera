994b09ae2d1791a1229e1b733f6253e6f4ca316e
gregfieler <gregfieler@gmail.com>
2014-12-09 21:37:49 -0800

    added comments

--- corr.R
+++ corr.R
@@ -1,14 +1,15 @@
 corr <- function(directory="specdata", threshold=0){
   ## Return numeric vector of correlations  
+  
+  ## 'directory' is where to find the data, 'threshold' is min # of pairs to accept for cor
 
   corVec <- numeric(0)
   obsCnt <- numeric(0)
   x <- 1
   while (x < 333) {  
-    ##  get the file name  
+    ##  get the file name  (there's probably a better way to do this)
     if (x < 10) {
-      fileName <- paste(directory,"/","00",x,".csv",sep="")
-    
+      fileName <- paste(directory,"/","00",x,".csv",sep="") 
       } else { 
       if (x < 100) {
         fileName <- paste(directory,"/","0",x,".csv",sep="")
@@ -19,7 +20,7 @@ corr <- function(directory="specdata", threshold=0){
     ##  read the csv file
     pData <- read.csv(fileName)
     
-    ##  count complete cases
+    ##  count complete cases (keeping vector for debugging)
     obsCnt[x] <- sum(complete.cases(pData))
     
     ##  if nobCnt is over the threshold perform the corr
